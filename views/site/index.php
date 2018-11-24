1f1f5b3b7f49892b58f05f5d60bd5b0a4d3d1c3c
kevin0217 <kevin0217@126.com>
2018-11-24 22:56:21 +0800

    '优化样式代码'

--- views/site/index.php
+++ views/site/index.php
@@ -14,10 +14,10 @@ $ct = yiiParams('ct');
         <div class="col-xs-12 col-sm-12 text-center" style="width: 100%; text-align: center">
             <ul id="myTab" class="nav nav-tabs sban">
                 <li class="col-xs-12 col-sm-2"></li>
-                <li class="active col-xs-12 col-sm-4"><a href="#service" data-toggle="tab"><img src="/img/ban1.jpg"></a><div class="title col-xs-10 col-sm-10 text-center"><?=Yii::t('home','Shipping')?></div></li>
+                <li class="active col-xs-6 col-sm-4"><a href="#service" data-toggle="tab"><img src="/img/ban1.jpg"></a><div class="title col-xs-10 col-sm-10 text-center"><a href="#service" data-toggle="tab"><?=Yii::t('home','Shipping')?></a></div></li>
 <!--                <li class="col-xs-12 col-sm-1"></li>-->
 
-                <li class="col-xs-12 col-sm-4"><a href="#product" data-toggle="tab"><img src="/img/ban2.jpg"></a><div class="title col-xs-10 col-sm-10 text-center"><?=Yii::t('home','Service')?></div></li>
+                <li class="col-xs-6 col-sm-4"><a href="#product" data-toggle="tab"><img src="/img/ban2.jpg"></a><div class="title col-xs-10 col-sm-10 text-center"><a href="#product" data-toggle="tab"><?=Yii::t('home','Service')?></a></div></li>
             </ul>
         </div>
         <div class="clear"></div>
@@ -31,7 +31,7 @@ $ct = yiiParams('ct');
                         <?php foreach($service as $k=>$v):
                             $b = $k%4;
                             ?>
-                            <li class="<?=$ct[$b]?> col-xs-12 col-sm-4">
+                            <li class="<?=$ct[$b]?> col-xs-6 col-sm-4">
                                 <p class="pic">
                                     <a href="<?php echo yiiUrl('/site/service-detail?id='.$v['id'])?>">
                                         <img src="<?= empty($v['imgUrl'])?'/img/logo.png':$v['imgUrl'];?>" alt="<?=$v['name']?>">
@@ -55,7 +55,7 @@ $ct = yiiParams('ct');
                     <?php foreach($product as $k=>$v):
                         $b = $k%4;
                         ?>
-                        <li class="<?=$ct[$b]?> col-xs-12 col-sm-4">
+                        <li class="<?=$ct[$b]?> col-xs-6 col-sm-4">
                             <p class="pic">
                                 <a href="<?php echo yiiUrl('/site/product-detail?id='.$v['id'])?>">
                                     <img src="<?= empty($v['imgUrl'])?'/img/logo.png':$v['imgUrl'];?>" alt="<?=$v['name']?>">
@@ -88,14 +88,14 @@ $ct = yiiParams('ct');
                 echo '<div class="key col-xs-12 col-sm-12"><ul>';
                 $ls = explode(',',$about['hkey']);
                 foreach ($ls as $l) {
-                    echo "<li>$l</li>";
+                    echo "<li class='col-xs-5 col-md-2 text-center'>$l</li>";
                 }
                 echo '</ul></div><div class="clear"></div>';
             }
             ?>
 
-            <div class="des col-xs-12 col-sm-6"><?=$about['desc']?></div>
-            <div class="pic col-xs-12 col-sm-6"><a href="<?=$about['video']?>" target="_blank"><img src="/img/pc.png"></a></div>
+            <div class="pic col-xs-12 col-sm-6 fr"><a href="<?=$about['video']?>" target="_blank"><img src="/img/pc.png"></a></div>
+            <div class="des col-xs-12 col-sm-6 fl"><?=$about['desc']?></div>
             <div class="more-btn col-xs-12 col-sm-12"><a href="<?=yiiUrl('site/about')?>" class="more"><?=Yii::t('home','More')?></a> </div>
             <div class="clear"></div>
         </div>
@@ -109,11 +109,11 @@ $ct = yiiParams('ct');
                 <?php foreach($news as $v): ?>
                     <li class=" col-xs-12 col-sm-6">
                         <a href="<?php echo yiiUrl('/site/news-detail?id='.$v['id'])?>">
-                            <p class="pic col-xs-12 col-sm-4">
+                            <p class="pic col-xs-6 col-sm-4">
                                 <img src="<?= empty($v['imgUrl'])?'/img/logo.png':$v['imgUrl'];?>" alt="<?=$v['name']?>" class="bg">
                                 <img src="<?= empty($v['imgUrl'])?'/img/logo.png':$v['imgUrl'];?>" alt="<?=$v['name']?>" class="picture">
                             </p>
-                            <span class="con col-xs-12 col-sm-8">
+                            <span class="con col-xs-6 col-sm-8">
 <!--    							<h3>--><?//=empty($v['created_at'])?'':date('m-d',$v['created_at']) ?><!--</h3>-->
     							<h2><?=$v['name']?></h2>
     							<div class="font">
